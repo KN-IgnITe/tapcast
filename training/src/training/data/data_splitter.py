c931fa4a98b29d1fa45ec69f8cf198c6aca94ccd
Jakub Bogacz <jakubbogacz05@gmail.com>
2026-04-07 18:45:59 +0200

    feat( walk-forward validation): added expanding window

--- training/src/training/data/data_splitter.py
+++ training/src/training/data/data_splitter.py
@@ -1,23 +1,38 @@
-from typing import Tuple
-
+from typing import Iterator, Tuple
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import TimeSeriesSplit
 class DataSplitter:
     """Splits data into chronological train and test dataframes based on a specified test size percentage."""
 
     test_size: float
+    n_splits: int
 
-    def __init__(self, test_size: float = 0.2):
+    def __init__(self, test_size: float = 0.15, n_splits: int = 5):
         """
         :param test_size: Proportion of the dataset to include in the test split (between 0 and 1).
+        :param n_splits: Number of splits to create.
         """
         self.test_size = test_size
+        self.n_splits = n_splits
 
-    def split(self, df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    def get_final_test_split(self, df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """Cuts last x of data for final test set"""
         df_sorted = df.sort_values(by="date").reset_index(drop=True)
 
         split_idx = int(len(df_sorted) * (1 - self.test_size))
 
-        train_df = df_sorted.iloc[:split_idx].copy()
+        train_val_df = df_sorted.iloc[:split_idx].copy()
         test_df = df_sorted.iloc[split_idx:].copy()
 
-        return train_df, test_df
+        return train_val_df, test_df
+            
+
+    def get_walk_forward_splits(self, df: pd.DataFrame) -> Iterator[Tuple[np.ndarray, np.ndarray]]:
+        """
+        Generates the forward window of data,
+        expects sorted data by date
+        """
+        tscv = TimeSeriesSplit(n_splits=self.n_splits)
+
+        return tscv.split(df)
