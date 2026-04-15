df74d2f8f4f0c73ebcf998c43b34332d07dac250
Jakub Bogacz <jakubbogacz05@gmail.com>
2026-04-15 08:28:33 +0200

    fix(imporst): fix imports in main and types

--- training/src/training/main.py
+++ training/src/training/main.py
@@ -1,14 +1,16 @@
 from pathlib import Path
-from data.data_loader import DemandDataLoader
-from data.data_splitter import DataSplitter
-from models.model_trainer import ModelTrainer
 
-def main():
+from training.data.data_loader import DemandDataLoader
+from training.data.data_splitter import DataSplitter
+from training.models.model_trainer import ModelTrainer, ModelType
+
+
+def main() -> None:
     data_path = Path(__file__).parents[2] / "data" / "raw" / "mocked_data.json"
-    
+
     loader = DemandDataLoader(data_path)
-    splitter = DataSplitter(test_size=0.15, n_splits=5) # 85% train, 15% test
-    
+    splitter = DataSplitter(test_size=0.15, n_splits=5)  # 85% train, 15% test
+
     # 1. Load data and process
     print("==========Start of pipeline============")
     df_flat = loader.load_and_process()
@@ -20,24 +22,24 @@ def main():
 
     # 3. Train and evaluate model
 
-    trainer_log_lin = ModelTrainer(model_type="log_lin")
+    trainer_log_lin = ModelTrainer(model_type=ModelType.LOG_LIN)
     mae_log_lin = trainer_log_lin.run_walk_forward_training(train_val_df, splitter)
 
-    trainer_xgboost = ModelTrainer(model_type="xgboost")
+    trainer_xgboost = ModelTrainer(model_type=ModelType.XGBOOST)
     mae_xgboost = trainer_xgboost.run_walk_forward_training(train_val_df, splitter)
 
     print(f"Log-linear model MAE: {mae_log_lin:.4f}")
     print(f"XGBoost model MAE: {mae_xgboost:.4f}")
 
-
-    # 4. Final evaluation on test set 
+    # 4. Final evaluation on test set
     print("\n==========Final evaluation on test set============")
-    
+
     final_mae_log_lin = trainer_log_lin.evaluate_on_test(train_val_df, test_df)
     final_mae_xgboost = trainer_xgboost.evaluate_on_test(train_val_df, test_df)
 
     print(f"Log-Linear model final MAE on test set: {final_mae_log_lin:.4f}")
     print(f"XGBoost model final MAE on test set: {final_mae_xgboost:.4f}")
+
 
 if __name__ == "__main__":
     main()
