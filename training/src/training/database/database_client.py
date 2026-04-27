c5905c755cbfe8e1dc665e62985a1c0de7003825
Ziuteks <ziutekxd56@gmail.com>
2026-04-27 09:28:30 +0200

    test(training): add tests for db connection

--- training/src/training/database/database_client.py
+++ training/src/training/database/database_client.py
@@ -1,5 +1,6 @@
 import psycopg
 from psycopg.rows import dict_row
+from types import TracebackType
 from typing import List, LiteralString, Optional
 
 from training.database.database_config import DatabaseConfig
@@ -9,7 +10,6 @@ class DatabaseClient:
     def __init__(self, config: DatabaseConfig) -> None:
         self._config = config
         self._connection: Optional[psycopg.Connection] = None
-
 
     def connect(self) -> bool:
         """Opens a connection only if it is closed."""
@@ -21,7 +21,6 @@ class DatabaseClient:
                 print(f"Connection error: {e}")
                 raise
         return True
-
 
     def close(self) -> None:
         """Closes the connection."""
@@ -35,17 +34,22 @@ class DatabaseClient:
         self.connect()
         return self  # Returns the client instance to the variable after 'as'
 
-
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
         """Called at the end of the 'with' block, regardless of errors."""
         if exc_type:
             print(f"An error occurred: {exc_val}")
         self.close()
 
-
     def fetch(self, query: LiteralString, params: Optional[tuple] = None) -> List[dict]:
         if not self._connection or self._connection.closed:
-            raise ConnectionError("No connection available. Use 'with DatabaseClient(...) as client'.")
+            raise ConnectionError(
+                "No connection available. Use 'with DatabaseClient(...) as client'."
+            )
 
         with self._connection.cursor(row_factory=dict_row) as cur:
             cur.execute(query, params)
