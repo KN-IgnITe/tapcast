64c33a0ef313214def0880c10738165889fee4e3
piotrkkskalisz <pawlaczyk.kopernik@gmail.com>
2026-07-26 10:45:08 +0200

    test(schedule): add integration tests

---
+++ backend/internal/scheduler/cron_test.go
@@ -0,0 +1,38 @@
+//go:build integration
+
+package scheduler
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/robfig/cron/v3"
+	"github.com/stretchr/testify/require"
+)
+
+type testJob struct {
+	called atomic.Bool
+}
+
+func (j *testJob) Run(ctx context.Context) {
+	j.called.Store(true)
+}
+
+func TestSchedulerRunJob(t *testing.T) {
+	job := &testJob{}
+
+	scheduler := &Scheduler{
+		cron: cron.New(),
+	}
+
+	scheduler.AddJob(job, "@every 1s")
+
+	scheduler.Start()
+	defer scheduler.Stop()
+
+	require.Eventually(t, func() bool {
+		return job.called.Load()
+	}, 5*time.Second, 100*time.Millisecond)
+}
